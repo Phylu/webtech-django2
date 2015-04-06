29e5bf0a40a785dddd3eb2663efde613452e0bcc
Janosch Maier <maierjanosch@gmail.com>
2015-04-06 15:56:19 +0200

    webtech skeleton vote_again url removed

--- webtech-skeleton/scorecard/urls.py
+++ webtech-skeleton/scorecard/urls.py
@@ -6,6 +6,5 @@ urlpatterns = patterns('',
                        url(r'^$', views.IndexView.as_view(), name='index'),
                        url(r'^vote/(?P<pk>\d+)/(?P<vote>-?\d)/$', views.vote, name='vote'),
                        url(r'^details/(?P<pk>\d+)$', views.details, name='details'),
-                       url(r'^vote_again$', views.vote_again, name='vote_again'),
                        url(r'^statistics', views.statistics, name="statistics"),
                        )
